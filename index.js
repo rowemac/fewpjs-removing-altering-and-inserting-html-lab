2eaebf3d64d5e35e0593b8c3c90c2135629dc63b
Mac Rowe <m.rowe42@gmail.com>
2021-05-24 16:47:24 -0400

    commit tree e8f9687fda29a2f879a9014d5ea3ba73267de4ee

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+main.remove(main.querySelector('main')) 
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Mac is the champion'
